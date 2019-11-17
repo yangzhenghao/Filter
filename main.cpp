33c5bd6f007b656ad36981f92bf002c1265a0815
Yang Zhenghao <1129195141@qq.com>
2019-11-17 23:12:57 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,10 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include <QDebug>
-#include <QDir>
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QDir temp("../resource/hh.png");
-    qDebug()<<QApplication::applicationFilePath();
-    qDebug()<<temp.absolutePath();
     MainWindow w;
     w.show();
 
